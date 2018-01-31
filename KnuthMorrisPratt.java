55df26ac7e1ed927046f94bcb3df856969963262
jspinak <32642391+jspinak@users.noreply.github.com>
2018-01-31 12:55:08 +0100

    Add files via upload

--- KnuthMorrisPratt.java
+++ KnuthMorrisPratt.java
@@ -19,6 +19,48 @@ class KnuthMorrisPratt {
         this.next2 = new int[m];
     }
 
+    public void print_next_tabelle() {
+        System.out.println("next Tabelle");
+        System.out.println("------------");
+        System.out.println("next wert = wieviele Stelle am Anfang des Musters sind genau wie das Submuster links von dieser Stelle");
+        print_tabelle(this.P,this.next);
+        System.out.println("next' Tabelle");
+        System.out.println("-------------");
+        System.out.println("setze next'[j] auf next[next[j]], wenn P[j] == P[next[j]]");
+        System.out.println("wenn diese beide chars gleich sind (P[j],P[next[j]]), wir wissen dass die Muster da nicht passen wird, suchen wir die nachste match");
+        print_tabelle(this.P,this.next2);
+    }
+
+    public void print_tabelle(String Muster, int[] werte) {
+        int i;
+        int menge = Muster.length();
+        int menge_ = menge*4 - 1;
+        System.out.print(" ");
+        for (i = 0; i < menge_; i++) {
+            System.out.print("_");
+        }
+        System.out.print("\n|");
+        for (i = 0; i < menge; i++) {
+            System.out.print(" "+Muster.charAt(i)+" |");
+        }
+        System.out.print("\n ");
+        for (i = 0; i < menge_; i++) {
+            System.out.print("_");
+        }
+        System.out.print("\n|");
+        for (i = 0; i < menge; i++) {
+            if (werte[i] >= 0) {
+                System.out.print(" ");
+            }
+            System.out.print(werte[i]+" |");
+        }
+        System.out.print("\n ");
+        for (i = 0; i < menge_; i++) {
+            System.out.print("_");
+        }
+        System.out.println("\n");
+    }
+
     public void mach_next_tabelle() {
         i = 0;
         j = -1;
@@ -30,38 +72,24 @@ class KnuthMorrisPratt {
                 next[i] = j;                 
             } else j = next[j];
         }
-        System.out.println("\nder next wert bedeutet: wieviele Stelle am Anfang des Musters sind genau wie das Submuster links von dieser Stelle");
-        System.out.println("______ next tabelle ______");
-        for (i=0; i<m; i++) {
-            System.out.print(P.charAt(i)+":"+next[i]+" ");
-        }
-        System.out.println("\n");
-        System.out.println("setze next'[j] auf next[next[j]], wenn P[j] == P[next[j]]");
-        System.out.println("wenn diese beide chars gleich sind (P[j],P[next[j]]), wir wissen dass die Muster da nicht passen wird, suchen wir die nachste match");
+        //System.out.println("setze next'[j] auf next[next[j]], wenn P[j] == P[next[j]]");
+        //System.out.println("wenn diese beide chars gleich sind (P[j],P[next[j]]), wir wissen dass die Muster da nicht passen wird, suchen wir die nachste match");
         i = 0;
         j = -1;
         next2[0] = -1;
         while (i < m-1) {
             if ((j==-1) || (P.charAt(i) == P.charAt(j))) {
-                //System.out.println("j==-1 || char(i)==char(j): "+i+" "+j);
                 i++;
                 j++;
-                //System.out.println("i++j++: "+i+" "+j);
                 if (P.charAt(i) == P.charAt(j)) {
                     next2[i] = next2[j];
-                    //System.out.println("char(i)==char(j): "+P.charAt(i)+" "+P.charAt(j));
                 }
                 else next2[i] = j;
             } else { 
                 j = next2[j];
-                //System.out.println("j=next2[j]: "+i+" "+j);
             }
         }
-        System.out.println("______ next' ______");
-        for (i=0; i<m; i++) {
-            System.out.print(P.charAt(i)+":"+next2[i]+" ");
-        }
-        System.out.println("\n");
+        print_next_tabelle();
     }
 
     public int vergleich_an_der_stelle(int stelle) {
@@ -71,14 +99,14 @@ class KnuthMorrisPratt {
         for (counter=0; counter<leerzeichen; counter++) {
             System.out.print(" ");
         }
-        System.out.println(P);
+        System.out.println(P); //print neben dem Text wo wir vergleichen werden
         i = stelle;
         char t = T.charAt(i);
         char p = P.charAt(0);
         for (j=0; j<m; j++) {
             t = T.charAt(i+j);
             p = P.charAt(j);
-            if (t != p) break; 
+            if (t != p) break; //kein match  
         }
         if (j == m) {
             System.out.println("match");
@@ -87,18 +115,20 @@ class KnuthMorrisPratt {
         } else {
             if (stelle == T.length()-P.length()) return stelle+1;
             int muster_verschiebung = j-next2[j]; 
-            for (counter=0; counter<j; counter++) {
+            for (counter=0; counter<(i+j); counter++) {
                 System.out.print(" ");
             }
             System.out.println("|");
             System.out.println("kein match @ Stelle:"+j+" Text:"+t+" Muster:"+p);
-            System.out.println("verschieb zu Musterstelle next'("+j+") = "+next2[j]+" = "+muster_verschiebung+" Stellen");
+            System.out.println("verschieb zu Musterstelle next'("+j+") = "+next2[j]+" = "+muster_verschiebung+" Stellen \n");
             return (stelle + muster_verschiebung);
         }
     }
 
     public void matches_finden() {
         int stelle = 0;
+        System.out.println("vergleichen und verschieben");
+        System.out.println("---------------------------");
         while (stelle < T.length()-P.length()) {
             stelle = this.vergleich_an_der_stelle(stelle);
         }
@@ -106,6 +136,7 @@ class KnuthMorrisPratt {
 
     public static void main(String[] args) {
         KnuthMorrisPratt kmp = new KnuthMorrisPratt(args[0],args[1]);
+        System.out.println("\n ********** KnuthMorrisPratt ********** \n");
         kmp.mach_next_tabelle();    
         kmp.matches_finden();
     }
